d58bda63fadaf92dc663bb94491f9b75c8862e84
thomaswparks <thomaswparks@users.noreply.github.com>
2026-01-17 03:46:14 +0000

    deploy: brothersinchrist/menofgod-content@53e2eea197669e5cc10efc03437a1823d25c54bf

--- tmp/jekyll-remote-theme-20260116-4020508-zdiv1f/assets/js/lunr/lunr-store.js
+++ tmp/jekyll-remote-theme-20260116-4020508-zdiv1f/assets/js/lunr/lunr-store.js
@@ -18,7 +18,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Episodes",
-        "excerpt":"                                                                                 Why Most Christians Get Discipleship Wrong                   January 08, 2026                                                                              Your browser does not support the audio element.                                Hard Men Podcast                   July 09, 2025        • Eric Conn                   Building Bands of Brothers: What This Looks Like in Ogden               ","categories": [],
+        "excerpt":"   Episodes                                                                                     Why Most Christians Get Discipleship Wrong                   January 08, 2026                                                                              Your browser does not support the audio element.                                Hard Men Podcast                   July 09, 2025        • Eric Conn                   Building Bands of Brothers: What This Looks Like in Ogden                ","categories": [],
         "tags": [],
         "url": "/menofgod-site/episodes/",
         "teaser": null
