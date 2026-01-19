cf878dd6add0dfb4c5707878e444bf27f9f5842d
thomaswparks <thomaswparks@users.noreply.github.com>
2026-01-19 02:28:31 +0000

    deploy: brothersinchrist/menofgod-content@313ee0fa604421285c08cf1e808cd12aa29ce170

--- tmp/jekyll-remote-theme-20260116-4018520-mdkys5/assets/js/lunr/lunr-store.js
+++ tmp/jekyll-remote-theme-20260116-4018520-mdkys5/assets/js/lunr/lunr-store.js
@@ -42,7 +42,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Partner Organizations",
-        "excerpt":"Partner Organizations These are the ministries, networks, and organizations we stand alongside. Caring Solutions Hope Offered. Futures Changed. Visit Website Samaritan's Purse Samaritan's Purse brings physical relief and the hope of the Gospel to hurting people around the world. Visit Website The Salvation Army - Warner Robins Corp Wherever there...","categories": [],
+        "excerpt":"Partner Organizations These are the ministries, networks, and organizations we stand alongside. Samaritan's Purse Samaritan's Purse brings physical relief and the hope of the Gospel to hurting people around the world. Visit Website The Salvation Army - Warner Robins Corp Wherever there is a need in warner robins, georgia you'll...","categories": [],
         "tags": [],
         "url": "/menofgod-site/partners/",
         "teaser": null
