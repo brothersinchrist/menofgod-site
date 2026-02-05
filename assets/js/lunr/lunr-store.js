86e6d63336683927179e91e8ccd2180112ccd619
thomaswparks <thomaswparks@users.noreply.github.com>
2026-02-05 04:32:20 +0000

    deploy: brothersinchrist/menofgod-content@aca1bbf40725731ff1c52ccc5974663855e233fe

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2609,12 +2609,6 @@ var store = [{
         "url": "/menofgod-site/webparts/about/story.html",
         "teaser": null
       },{
-        "title": "Test",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/menofgod-site/webparts/about/test.html",
-        "teaser": null
-      },{
         "title": "00 Hero",
         "excerpt":" ","categories": [],
         "tags": [],
